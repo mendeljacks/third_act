8f522ed59fc1258b54151b5a2d4ed9aac3c4df2c
Mendel <mendeljacks@gmail.com>
2021-06-02 12:37:59 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,95 @@
+const {Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar, TransferTransaction, TokenMintTransaction, TokenCreateTransaction} = require("@hashgraph/sdk");
+require("dotenv").config();
+
+async function main() {
+
+    //Grab your Hedera testnet account ID and private key from your .env file
+    const myAccountId = process.env.MY_ACCOUNT_ID;
+    const myPrivateKey = process.env.MY_PRIVATE_KEY;
+
+
+    const privateKey = await PrivateKey.generate();
+    const publicKey = privateKey.publicKey;
+    // const myPublicKey = process.env.MY_PUBLIC_KEY;
+
+    // If we weren't able to grab it, we should throw a new error
+    if (myAccountId == null ||
+        myPrivateKey == null) {
+        throw new Error("Environment variables myAccountId and myPrivateKey must be present");
+    }
+
+    // Create our connection to the Hedera network
+    // The Hedera JS SDK makes this really easy!
+    const client = Client.forTestnet();
+
+    client.setOperator(myAccountId, myPrivateKey);
+
+    //Create new keys
+    const newAccountPrivateKey = await PrivateKey.generate();
+    const newAccountPublicKey = newAccountPrivateKey.publicKey;
+
+    //Create a new account with 1,000 tinybar starting balance
+    const newAccountTransactionResponse = await new AccountCreateTransaction()
+        .setKey(newAccountPublicKey)
+        .setInitialBalance(Hbar.fromTinybars(1000))
+        .execute(client);
+
+    // Get the new account ID
+    const getReceipt = await newAccountTransactionResponse.getReceipt(client);
+    const newAccountId = getReceipt.accountId;
+
+    console.log("The new account ID is: " + newAccountId);
+
+    //Verify the account balance
+    const accountBalance = await new AccountBalanceQuery()
+        .setAccountId(newAccountId)
+        .execute(client);
+
+    console.log("The new account balance is: " + accountBalance.hbars.toTinybars() + " tinybar.");
+
+    //Create the transfer transaction
+    const transferTransactionResponse = await new TransferTransaction()
+        .addHbarTransfer(myAccountId, Hbar.fromTinybars(-1000))
+        .addHbarTransfer(newAccountId, Hbar.fromTinybars(1000))
+        .execute(client);
+
+    //Verify the transaction reached consensus
+    const transactionReceipt = await transferTransactionResponse.getReceipt(client);
+    console.log("The transfer transaction from my account to the new account was: " + transactionReceipt.status.toString());
+
+    //Check the new account's balance
+    const getNewBalance = await new AccountBalanceQuery()
+        .setAccountId(newAccountId)
+        .execute(client);
+
+    console.log("The account balance after the transfer is: " + getNewBalance.hbars.toTinybars() + " tinybar.")
+
+    const transaction = await new TokenCreateTransaction()
+        .setTokenName('This is my token name')
+        .setTokenSymbol('TT')
+        .setTreasuryAccountId(myAccountId)
+        .setInitialSupply(1)
+        .setAdminKey(publicKey)
+        .freezeWith(client)
+    // .setMaxTransactionFee(new HBar(30))
+
+    //Sign the transaction with the token adminKey and the token treasury account private key
+    const signTx = await (await transaction.sign(privateKey)).sign(privateKey);
+   
+
+    //Sign the transaction with the client operator private key and submit to a Hedera network
+    const txResponse = await signTx.execute(client);
+
+    //Get the receipt of the transaction
+    const receipt = await txResponse.getReceipt(client);
+
+    //Get the token ID from the receipt
+    const tokenId = receipt.tokenId;
+
+    console.log("The new token ID is " + tokenId);
+
+
+    debugger
+
+}
+main();
