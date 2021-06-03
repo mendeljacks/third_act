fbabeacecc8b8fbaa8c7d611bb034814c513c8d4
Mendel <mendeljacks@gmail.com>
2021-06-03 14:33:28 -0400

    Working prototype

--- index.js
+++ index.js
@@ -1,97 +1,61 @@
-const {Client, PrivateKey, AccountId, AccountCreateTransaction, AccountBalanceQuery, Hbar, TransferTransaction, TokenMintTransaction, TokenCreateTransaction} = require("@hashgraph/sdk");
+const {Client, PrivateKey, AccountId, AccountCreateTransaction, AccountBalanceQuery, Hbar, TransferTransaction, TokenMintTransaction, TokenCreateTransaction, TokenAssociateTransaction} = require("@hashgraph/sdk");
 
 
-module.exports.mint_token = async ({customer_account_id, customer_private_key, tinybars, token: {name, initial_supply, symbol}}, operator_account_id, operator_private_key) => {
+module.exports.mint_token = async ({customer_account_id, customer_private_key, tinybars, net, token: {name, initial_supply, symbol}}, operator_account_id, operator_private_key) => {
 
-    const client = Client.forTestnet();
-    client.setOperator(operator_account_id, operator_private_key);
+    const fornet = net === 'main' ? 'forMainnet' : 'forTestnet'
 
-///---------------Temporary--------------------------
-    const temp_customer_private_key = await PrivateKey.generate()
-  
-    // Create a new account with 1,000 tinybar starting balance
-    const newAccountTransactionResponse = await new AccountCreateTransaction()
-        .setKey(temp_customer_private_key.publicKey)
-        .setInitialBalance(Hbar.fromTinybars(1000))
-        .execute(client);
+    const operator_client = Client[fornet]();
+    operator_client.setOperator(operator_account_id, operator_private_key);
 
-    // Get the new account ID
-    const getReceipt = await newAccountTransactionResponse.getReceipt(client);
-    const temp_customer_account_id = getReceipt.accountId;
+    const customer_client = Client[fornet]();
+    customer_client.setOperator(customer_account_id, customer_private_key)
 
-    console.log("The new account ID is: " + temp_customer_account_id);
+    const customerPublicKey = customer_private_key.publicKey
 
-    // Verify the account balance
-    const accountBalance = await new AccountBalanceQuery()
-        .setAccountId(temp_customer_account_id)
-        .execute(client);
-
-    console.log("The new account balance is: " + accountBalance.hbars.toTinybars() + " tinybar.");
-
-
-///---------------Temporary--------------------------
-
-    // Customer keys
-    // const customerPrivateKey = await PrivateKey.fromString(customer_private_key)
-
-    const customerPublicKey = temp_customer_private_key.publicKey //customerPrivateKey.publicKey;
-    
     // Operator keys
     const operatorPrivateKey = await PrivateKey.fromString(operator_private_key);
     const operatorPublicKey = operatorPrivateKey.publicKey;
 
 
-    // // If we weren't able to grab it, we should throw a new error
-    // if (myAccountId == null ||
-    //     myPrivateKey == null) {
-    //     throw new Error("Environment variables myAccountId and myPrivateKey must be present");
-    // }
-
-    // Create our connection to the Hedera network
-    // The Hedera JS SDK makes this really easy!
-
-        ///--------------------temp
-    // Check the new account's balance
+    ///--------------------temp
     const getNewBalance = await new AccountBalanceQuery()
         .setAccountId(operator_account_id)
-        .execute(client);
+        .execute(customer_client);
 
-    console.log("The account balance after the transfer is: " + getNewBalance.hbars.toTinybars() + " tinybar.")
+    console.log("Operator balance " + getNewBalance.hbars.toTinybars() + " tinybar.")
 
-    // Check the new account's balance
     const getNewBalance2 = await new AccountBalanceQuery()
-        .setAccountId(temp_customer_account_id)
-        .execute(client);
+        .setAccountId(customer_account_id)
+        .execute(customer_client);
 
-    console.log("The account balance after the transfer is: " + getNewBalance2.hbars.toTinybars() + " tinybar.")
+    console.log("Customer balance " + getNewBalance2.hbars.toTinybars() + " tinybar.")
     ///--------------------temp
 
 
 
     //Create the transfer transaction
     const transferTransactionResponse = await new TransferTransaction()
-        .addHbarTransfer(operator_account_id, Hbar.fromTinybars(-tinybars))
-        .addHbarTransfer(temp_customer_account_id, Hbar.fromTinybars(tinybars))
-        .execute(client);
+        .addHbarTransfer(customer_account_id, Hbar.fromTinybars(-tinybars))
+        .addHbarTransfer(operator_account_id, Hbar.fromTinybars(tinybars))
+        .execute(customer_client);
 
     //Verify the transaction reached consensus
-    const transactionReceipt = await transferTransactionResponse.getReceipt(client);
+    const transactionReceipt = await transferTransactionResponse.getReceipt(operator_client);
     console.log("The transfer transaction from my account to the new account was: " + transactionReceipt.status.toString());
 
     ///--------------------temp
-    // Check the new account's balance
     const getNewBalance3 = await new AccountBalanceQuery()
         .setAccountId(operator_account_id)
-        .execute(client);
+        .execute(customer_client);
 
-    console.log("The account balance after the transfer is: " + getNewBalance3.hbars.toTinybars() + " tinybar.")
+    console.log("After transfer operator balance " + getNewBalance3.hbars.toTinybars() + " tinybar.")
 
-    // Check the new account's balance
     const getNewBalance4 = await new AccountBalanceQuery()
-        .setAccountId(temp_customer_account_id)
-        .execute(client);
+        .setAccountId(customer_account_id)
+        .execute(customer_client);
 
-    console.log("The account balance after the transfer is: " + getNewBalance4.hbars.toTinybars() + " tinybar.")
+    console.log("After transfer customer balance " + getNewBalance4.hbars.toTinybars() + " tinybar.")
     ///--------------------temp
 
 
@@ -103,23 +67,77 @@ module.exports.mint_token = async ({customer_account_id, customer_private_key, t
         .setTreasuryAccountId(operator_account_id)
         .setInitialSupply(initial_supply)
         .setAdminKey(operatorPublicKey)
-        .freezeWith(client)
+        .freezeWith(operator_client)
     // .setMaxTransactionFee(new HBar(30))
 
     //Sign the transaction with the token adminKey and the token treasury account private key
     const signTx = await (await transaction.sign(operatorPrivateKey)).sign(operatorPrivateKey);
-   
+
 
     //Sign the transaction with the client operator private key and submit to a Hedera network
-    const txResponse = await signTx.execute(client);
+    const txResponse = await signTx.execute(operator_client);
 
     //Get the receipt of the transaction
-    const receipt = await txResponse.getReceipt(client);
+    const receipt = await txResponse.getReceipt(operator_client);
 
     //Get the token ID from the receipt
     const tokenId = receipt.tokenId;
 
-    console.log("The new token ID is " + tokenId);
+    console.log("A token has been created id: " + tokenId);
+
+
+
+
+
+
+
+
+    //Associate a token to an account and freeze the unsigned transaction for signing
+    const transaction3 = await new TokenAssociateTransaction()
+        .setAccountId(customer_account_id)
+        .setTokenIds([tokenId])
+        .freezeWith(customer_client);
+
+    //Sign with the private key of the account that is being associated to a token 
+    const signTx3 = await transaction3.sign(customer_private_key);
+
+    //Submit the transaction to a Hedera network    
+    const txResponse3 = await signTx3.execute(customer_client);
+
+    //Request the receipt of the transaction
+    const receipt3 = await txResponse3.getReceipt(customer_client);
+
+    //Get the transaction consensus status
+    const transactionStatus3 = receipt3.status;
+
+    console.log("The transaction consensus status " + transactionStatus3.toString());
+
+
+
+
+
+
+
+
+    const transfer_token_transaction = await new TransferTransaction()
+        .addTokenTransfer(tokenId, operator_account_id, -1)
+        .addTokenTransfer(tokenId, customer_account_id, 1)
+        .freezeWith(customer_client)
+
+    //Sign with the sender account private key
+    const signTx2 = await transfer_token_transaction.sign(operatorPrivateKey);
+
+    //Sign with the client operator private key and submit to a Hedera network
+    const txResponse2 = await signTx2.execute(customer_client);
+
+    //Request the receipt of the transaction
+    const receipt2 = await txResponse2.getReceipt(customer_client);
+
+    //Obtain the transaction consensus status
+    const transactionStatus2 = receipt2.status;
+
+    console.log("The transaction consensus status " + transactionStatus2.toString());
+
 
     return tokenId
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const {serializeError} = require('serialize-error')
 const joi = require('joi');
+const {PrivateKey, Client, AccountCreateTransaction, AccountBalanceQuery, Hbar} = require("@hashgraph/sdk");
 const port = process.env.PORT || 3000
 
 app.use(cors())
@@ -22,6 +23,7 @@ const token_schema = joi.object({
     customer_account_id: joi.any().required(),
     customer_private_key: joi.string().min(10).required(),
     tinybars: joi.number().positive().integer().required(),
+    net: joi.valid('test', 'main').required(),
     token: joi.object({
         name: joi.string().required(),
         symbol: joi.string().required(),
@@ -31,13 +33,43 @@ const token_schema = joi.object({
 
 app.post('/token', async (req, res) => {
 
+    // Validate customer request
     const {error, value} = token_schema.validate(req.body);
     if (error) {
         return res.status(400).json(error)
     }
 
+    // Extract variables from environment
     const operator_account_id = process.env.MY_ACCOUNT_ID
     const operator_private_key = process.env.MY_PRIVATE_KEY
+
+
+    ///---------------Temporary make a test account on testnet--------------------------
+    const temp_customer_private_key = await PrivateKey.generate()
+    const client = Client['forTestnet']();
+    client.setOperator(operator_account_id, operator_private_key);
+    // Create a new account with 1,000 tinybar starting balance
+    const newAccountTransactionResponse = await new AccountCreateTransaction()
+        .setKey(temp_customer_private_key.publicKey)
+        .setInitialBalance(Hbar.fromTinybars(30*1000000000))
+        .execute(client);
+
+    // Get the new account ID
+    const getReceipt = await newAccountTransactionResponse.getReceipt(client);
+    const temp_customer_account_id = getReceipt.accountId;
+
+    console.log("The new account ID is: " + temp_customer_account_id);
+
+    // Verify the account balance
+    const accountBalance = await new AccountBalanceQuery()
+        .setAccountId(temp_customer_account_id)
+        .execute(client);
+
+    console.log("The new account balance is: " + accountBalance.hbars.toTinybars() + " tinybar.");
+
+    req.body.customer_private_key = temp_customer_private_key
+    req.body.customer_account_id = temp_customer_account_id
+    ///---------------Temporary--------------------------
 
     await mint_token(req.body, operator_account_id, operator_private_key)
         .then(result => res.status(200).json(result))
